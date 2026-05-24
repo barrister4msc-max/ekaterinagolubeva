a6690f3cb12186ea134c3021713211b51300d9c0
barrister4msc-max <barrister.4.msc@gmail.com>
2026-05-24 04:19:25 +0400

    Update workspace.crm.tsx

--- src/routes/workspace.crm.tsx
+++ src/routes/workspace.crm.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/crm")({
+export const Route = createFileRoute("/workspace/crm")({
   component: CRMPage,
 });
 
